9b2d721d3b226fa0f2f4859147b1310b8710031e
ETester2022 <gekashopicin@gmail.com>
2023-05-28 18:39:37 +0500

    Домашка №1

---
+++ lesson_1_task_7.py
@@ -0,0 +1,46 @@
+def func1():
+    print('1')
+
+def func2():
+    print('2')
+
+def func3():
+    print('3')
+
+def func4():
+    print('4')
+
+def func5():
+    print('5')
+
+def func6():
+    print('6')
+
+def func7():
+    print('7')
+
+def func8():
+    print('8')
+
+def func9():
+    print('9')
+
+def func10():
+    print('0')
+
+def total():
+    func8()
+    func8()
+    func10()
+    func10()
+    func5()
+    func5()
+    func5()
+    func3()
+    func5()
+    func3()
+    func5()
+
+total()
+
+#88005553535
---
+++ lesson_1_task_8.py
@@ -0,0 +1,16 @@
+def param(num):
+    print(num)
+
+param(8)
+param(8)
+param(0)
+param(0)
+param(5)
+param(5)
+param(5)
+param(3)
+param(5)
+param(3)
+param(5)
+
+#88005553535
