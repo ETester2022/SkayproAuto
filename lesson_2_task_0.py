cea534e0539f1c6c1788bbd5f1b81561d3c67225
ETester2022 <gekashopicin@gmail.com>
2023-06-03 14:43:13 +0500

    Половина второй домашки

---
+++ lesson_2_task_0.py
@@ -0,0 +1,8 @@
+my_age = input()
+print ('Ваш возраст: ' + my_age)
+
+#my_age = input()
+#print ('Ваш возраст: ',  my_age + 1)
+
+my_age = int("23")
+print(my_age + 1)
---
+++ lesson_2_task_3.py
@@ -0,0 +1,10 @@
+def square(side):
+
+    if side % 2 == 0:
+        print(int(side) * int(side))
+    elif side % 2 == 1:
+        print(int(side) * int(side))
+    else:
+        print(int(side * side + 1))
+
+square(1.1)
