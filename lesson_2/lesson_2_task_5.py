29b5db3e4b4f25d84aea19aba75e04dbb37645d5
ETester2022 <gekashopicin@gmail.com>
2023-06-05 21:52:16 +0500

    Еще два задания

---
+++ lesson_2/lesson_2_task_5.py
@@ -0,0 +1,17 @@
+def month_to_season(num):
+
+    for num in range(1, 13):
+
+        if (num > 2 and num < 6):
+            print('Весна')
+        elif (num > 5 and num < 9):
+            print('Лето')
+        elif (num > 8 and num < 12):
+            print('Осень')
+        else:
+            print('Зима')
+    
+month_to_season(3)
+
+# Цикл использовал для того чтоб ограничить ввод количеством месяцев в году
+# Идей для решения нет
