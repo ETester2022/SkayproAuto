5ada3f494968def3f506e7c55ae82e47039c4225
ETester2022 <gekashopicin@gmail.com>
2023-06-09 10:27:58 +0500

    Все готово!

--- lesson_2/lesson_2_task_8.py
+++ lesson_2/lesson_2_task_8.py
@@ -1,4 +1,9 @@
 
-for x in range(18, 1, -4):
-    print(x, end=' ')
-    
+#for x in range(18, 1, -4):
+#    print( x, end=' ')
+
+numbers = range(18, 1, -4)
+
+numbers_list = list(numbers)
+
+print(numbers_list)
