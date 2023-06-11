5ec57fc2f78bd154ae153718328f06b28710aff1
ETester2022 <gekashopicin@gmail.com>
2023-06-11 14:29:10 +0500

    Правка от 11.06

--- lesson_2/lesson_2_task_5.py
+++ lesson_2/lesson_2_task_5.py
@@ -6,9 +6,9 @@ def month_to_season(num):
             print('Лето')
         elif (num > 8 and num < 12):
             print('Осень')
-        elif (num < 1 and num > 12):
+        elif (num < 1 or num > 12):
             print('Введено не верное значение!')
         else:
             print('Зима')
     
-month_to_season(1)
+month_to_season(13)
