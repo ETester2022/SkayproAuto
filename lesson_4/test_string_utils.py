41743d20d948f8ae66031d986c4bc5519ba11e0d
ETester2022 <gekashopicin@gmail.com>
2023-10-16 13:37:02 +0500

    Не получены данные

--- lesson_4/test_string_utils.py
+++ lesson_4/test_string_utils.py
@@ -1,17 +1,19 @@
 import pytest
 from string_utils import StringUtils
 
-string_utils = StringUtils()
-
 # Тест
 # тест 
 # 123
 # 04 апреля 2023
+# Пустая
+# " "
+# None
 
-def test_string_utils_capitilize_positive():
+pytest.mark.parametrize('data, result', [ ('Тест', 'Тест'), ('тест', 'Тест'), ('123', '123'), ('04 апреля 2023', '04 апреля 2023'), ('', ''), (' ', ' ') ])
+def test_string_utils_capitilize_positive(data, result):
     string_utils = StringUtils()
-    var1 = string_utils.capitilize('Тест')
-    assert var1 == 'Тест'
+    var1 = string_utils.capitilize(data)
+    assert var1 == result
 
 def test_string_utils_capitilize_and_positive():
     string_utils = StringUtils()
@@ -28,21 +30,18 @@ def test_string_utils_capitilize_date_positive():
     var1 = string_utils.capitilize('04 апреля 2023')
     assert var1 == '04 апреля 2023'
 
-# Пустая
-# " "
-# None
 
 def test_string_utils_capitilize_negative():
     string_utils = StringUtils()
-    with pytest.raises(AssertionError):
-        var1 = string_utils.capitilize('')
-        assert var1 == None
+#    with pytest.raises(AssertionError):
+    var1 = string_utils.capitilize('')
+    assert var1 == ''
 
 def test_string_utils_capitilize_space_negative():
     string_utils = StringUtils()
-    with pytest.raises(AssertionError):
-        var1 = string_utils.capitilize(' ')
-        assert var1 == None
+#    with pytest.raises(AssertionError):
+    var1 = string_utils.capitilize(' ')
+    assert var1 == ' '
 
 def test_string_utils_capitilize_empty_negative():
     string_utils = StringUtils()
