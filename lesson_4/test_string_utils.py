2986eec957abb63544a7342e314ba936f040be40
ETester2022 <gekashopicin@gmail.com>
2023-10-17 18:05:55 +0500

    Разделение на позитивные и негативные

--- lesson_4/test_string_utils.py
+++ lesson_4/test_string_utils.py
@@ -13,11 +13,18 @@ from string_utils import StringUtils
     ('Тест', 'Тест'), 
     ('тест', 'Тест'), 
     ('123', '123'), 
-    ('04 апреля 2023', '04 апреля 2023'), 
+    ('04 апреля 2023', '04 апреля 2023') 
+    ])
+def test_string_utils_capitilize_positive(data, result):
+    string_utils = StringUtils()
+    var1 = string_utils.capitilize(data)
+    assert var1 == result
+
+@pytest.mark.parametrize('data, result', [  
     ('', ''), 
     (' ', ' ') 
     ])
-def test_string_utils_capitilize_positive(data, result):
+def test_string_utils_capitilize_negative(data, result):
     string_utils = StringUtils()
     var1 = string_utils.capitilize(data)
     assert var1 == result
